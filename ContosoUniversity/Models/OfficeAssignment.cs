1cee3a68cc0771529753d939d5047e43e6fd67c4
jamesmaxwell <james.maxwell.hu@gmail.com>
2014-07-02 16:04:12 +0800

    add more function

---
+++ ContosoUniversity/Models/OfficeAssignment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class OfficeAssignment
+    {
+        [Key]
+        [ForeignKey("Instructor")]
+        public int InstructorID { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "办公地点")]
+        public string Location { get; set; }
+
+        public virtual Instructor Instructor { get; set; }
+    }
+}
--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,16 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "姓")]
+        [StringLength(50, MinimumLength = 1)]
         public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "名")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "名字长度不能超过50")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
+
+        [Display(Name = "注册日期")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "全名")]
+        public string FullName
+        {
+            get { return FirstMidName + "," + LastName; }
+        }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
